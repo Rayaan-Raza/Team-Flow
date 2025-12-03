d8f7b26ea3794272392d1a97fee9bd37e1665eb7
alimazhar <aleemazhar3005@gmail.com>
2025-12-04 03:59:28 +0500

    EmptyStates, Error Screens, Manifest fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
+    
+
     implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/teamflow/art/No_Internet_Connection.kt
+++ app/src/main/java/com/teamflow/art/No_Internet_Connection.kt
@@ -1,6 +1,7 @@
 package com.teamflow.art
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,7 @@ class No_Internet_Connection : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<ImageView>(R.id.btnBack).setOnClickListener { finish() }
     }
 }
